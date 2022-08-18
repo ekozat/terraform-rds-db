b8729db693e0a34e1e31558c54a3c4a1746afeee
emilykozat <emily.kozatchiner@jumbleberry.com>
2022-08-18 15:35:08 -0400

    Incorporated base information in main to create db cluster

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_rds_cluster" "default" {
+  cluster_identifier        = "master-dev-database-cluster"
+  engine                    = "aurora-mysql"
+  engine_version            = "5.6.mysql_aurora.1.20.1"
+  database_name             = "dev-db"
+  db_cluster_instance_class = "db.t3.medium"
+  master_username           = "sq_dance_adm"
+  master_password           = "***REMOVED***"
+}
