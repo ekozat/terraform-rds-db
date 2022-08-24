c4c11ae33e41bac711a430257e58d54d3d67b680
emilykozat <emily.kozatchiner@jumbleberry.com>
2022-08-24 17:23:47 -0400

    Added vpc security group IDs
    Need to make instance class modifiable

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ resource "aws_rds_cluster" "default" {
   engine                    = "aurora-mysql"
   engine_version            = "5.6.mysql_aurora.1.20.1"
   database_name             = "dev-db"
-  db_cluster_instance_class = "db.t3.medium"
+  //db_cluster_instance_class = "db.t3.medium" find a fix
   master_username           = "sq_dance_adm"
   master_password           = "***REMOVED***"
-  vpc_security_group_ids    = "RDS_DevWork"
+  vpc_security_group_ids    = ["RDS_DevWork"]
   //password authentication is enabled
 }
