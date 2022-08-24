838a6e7249404f39c12dabfeecde281eca618180
emilykozat <emily.kozatchiner@jumbleberry.com>
2022-08-24 17:20:13 -0400

    Ensures terraform provider has keys for aws

--- providers.tf
+++ providers.tf
@@ -13,6 +13,6 @@ terraform {
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
-  #access_key = var.AWS_ACCESS_KEY_ID
-  #secret_key = var.AWS_SECRET_ACCESS_KEY
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
