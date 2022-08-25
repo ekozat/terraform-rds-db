2daf3aee4ce6cdab1d953bca6db1a0283d327548
emilykozat <emily.kozatchiner@jumbleberry.com>
2022-08-25 11:43:33 -0400

    Created new instances defined within cluster

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
+resource "aws_rds_cluster_instance" "cluster_instances" {
+  count              = 2
+  cluster_identifier = aws_rds_cluster.master-dev-database-cluster.id
+  instance_class     = "db.t3.medium"
+  engine             = aws_rds_cluster.default.engine
+  engine_version     = aws_rds_cluster.default.engine_version
+
+}
+
 resource "aws_rds_cluster" "master-dev-database-cluster" {
   cluster_identifier        = "master-dev-database-cluster"
   engine                    = "aurora-mysql"
   engine_version            = "5.6.mysql_aurora.1.20.1"
   database_name             = "dev-db"
-  //db_cluster_instance_class = "db.t3.medium" find a fix
   master_username           = "sq_dance_adm"
   master_password           = "***REMOVED***"
   vpc_security_group_ids    = ["RDS_DevWork"]
